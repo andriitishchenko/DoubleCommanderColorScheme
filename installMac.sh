bfda8991f2cabd96ec556366bc7465fd3bfd1865
Andrii Tiischenko <atishch@MACMINI1309.local>
2016-12-28 21:06:14 +0200

    update

--- installMac.sh
+++ installMac.sh
@@ -2,9 +2,10 @@
 echo -e "\n\n"
 if [[ "$OSTYPE" != "darwin"* ]]; then
 	echo "This script works for MAC OSx only!"
+	echo "BYE!"
 	exit 1
 fi
-echo "Assume that you forked GIT to your arrount and probably wish to edit some configs."
+echo "Assume that you forked this to your GIT account and probably will wish to edit some configs."
 echo "This script will link config files from this folder to Double Commander so you can edit and share it."
 echo "Default path: ~/Library/Preferences/doublecmd/"
 echo "Do you wish to continue?"
@@ -25,6 +26,7 @@ fi
 
 destpath="$HOME/Library/Preferences/doublecmd/"
 thispath="$(pwd)/doublecmd/"
-ln -sf "$thispath/*.*" $destpath
 
+echo "Creating links ..."
+ln -sf "$thispath/*.*" $destpath
 echo "Done"
